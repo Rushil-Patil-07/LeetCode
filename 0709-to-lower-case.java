bf5a7fa9d13c5940cb0e6e15511759aee558b6a5
Rushil Patil <rushilpatil2067@gmail.com>
2026-08-11 22:59:23 +0530

    Add solutions for LC 258 (Add Digits) and LC 709 (To Lower Case)

---
+++ 0709-to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+
+    }
+    public static void main(String args[])
+    {
+        Solution sl = new Solution();
+        String result=sl.toLowerCase("RUSHIL");
+        System.out.println(result);
+    }
+}
