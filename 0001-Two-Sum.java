4156d066c9fee0ba9afbdabcd5edc44eb617d63c
Rushil Patil <rushilpatil2067@gmail.com>
2026-08-13 21:42:05 +0530

    add solution for LC 0001-(Two Sum) code

---
+++ 0001-Two-Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    for(int i=0;i<nums.length-1;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                return new int[]{i,j};
+            }
+        }
+    }
+    return null;
+    }
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        Solution solution = new Solution();
+        solution.twoSum(nums,13);
+    }
+}
