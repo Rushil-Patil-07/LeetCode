b14249856d5054f1676255d2403cdf70486893e4
Rushil Patil <rushilpatil2067@gmail.com>
2026-08-15 22:23:37 +0530

    add solution for LC 1662(check if two string arrays are equivalent)

---
+++ 1662-Check-if-Two-String-Arrays-are-Equavalent.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String sum1 = "";
+        String sum2 = "";
+        for (int i = 0; i < word1.length; i++) {
+            sum1 = sum1 + word1[i];
+        }
+        for (int i = 0; i < word2.length; i++) {
+            sum2 = sum2 + word2[i];
+        }
+        return sum1.equals(sum2);
+    }
+}
