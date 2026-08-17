b7893154cbc3c97cb526b99e182c83b87fd3185c
Rushil Patil <rushilpatil2067@gmail.com>
2026-08-17 20:15:29 +0530

    add solution for LC 0167(Two-Sum-II)

---
+++ 0167-Two-Sum-II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0, j=numbers.length-1,sum=0;
+        while(i<j)
+        {
+            sum=numbers[i]+numbers[j];
+            if(target>sum)
+            {
+                i++;
+            }
+            else if(target<sum)
+            {
+                j--;
+            }
+            else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+    return  new int[]{};
+    }
+}
