30f058ba28794036774ef44a51da6e07a704d2df
Rushil Patil <rushilpatil2067@gmail.com>
2026-08-18 20:03:18 +0530

    add solution for LC 1431(Kids with greatest number of candies)

---
+++ 1431-Kids-with-the-greatest-candies.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int max = 0;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int j = 0; j < candies.length; j++) {
+            if (candies[j] + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
