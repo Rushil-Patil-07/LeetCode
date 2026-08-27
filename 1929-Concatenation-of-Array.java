5a4acc971709c6392bc374394aba7bbf024c0f1d
Rushil Patil <rushilpatil2067@gmail.com>
2026-08-27 18:10:09 +0530

    add solution for LC 1929(Concatenation of Array)

---
+++ 1929-Concatenation-of-Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length * 2];
+        int i = 0;
+        while (i < nums.length) {
+            res[i] = nums[i];
+            res[i + nums.length] = nums[i];
+            i++;
+        }
+        return res;
+    }
+}
