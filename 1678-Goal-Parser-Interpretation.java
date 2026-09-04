a8afe0e93ca18f5b07701a7c44c7b569307e3184
Rushil Patil <rushilpatil2067@gmail.com>
2026-09-04 21:29:53 +0530

    add solution for LC 1678(Goal Parser Interpretation)

---
+++ 1678-Goal-Parser-Interpretation.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String interpret(String command) {
+        String ans = "";
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
+                ans = ans + "o";
+            } else if (command.charAt(i) == '(' && command.charAt(i + 1) == 'a') {
+                ans = ans + "al";
+            } else if (command.charAt(i) == 'G') {
+                ans = ans + "G";
+            }
+        }
+        return ans;
+    }
+}
