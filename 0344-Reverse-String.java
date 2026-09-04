b14c78bbcff367c67dbfdbd3b593a1111a77ec14
Rushil Patil <rushilpatil2067@gmail.com>
2026-09-04 21:07:30 +0530

    add solution for LC 344(Reverse String)

---
+++ 0344-Reverse-String.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+            char temp = s[j];
+            s[j] = s[i];
+            s[i] = temp;
+            i++;
+            j--;
+        }
+    }
+}
