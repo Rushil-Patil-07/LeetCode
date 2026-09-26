1801fa3d3993a711de91e091b61d5b68f731731c
Rushil Patil <rushilpatil2067@gmail.com>
2026-09-26 19:30:08 +0530

    add solution for LC 2824(Count Pairs Whose Sum is Less than Target)

---
+++ 2824-Count-Pairs-Whose-Sum-is-Less-than-Target.java
@@ -0,0 +1,16 @@
+import java.util.List;
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i==j) {
+                    continue;
+                }
+                if (nums.get(i) + nums.get(j) < target)
+                    count++;
+            }
+        }
+        return count/2;
+    }
+}
